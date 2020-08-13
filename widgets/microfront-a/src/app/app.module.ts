acf00e709feab73e470392617e948887c281e1e5
Lukasz Reszke <lure@infotjenester.no>
2020-08-13 19:18:33 +0200

    Added Angular Material and changed HelloWorldWidget HTMl

--- widgets/microfront-a/src/app/app.module.ts
+++ widgets/microfront-a/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, Injector } from '@angular/core';
 import { HelloWorldWidgetComponent } from './hello-world-widget/hello-world-widget.component';
 import { createCustomElement } from '@angular/elements';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 @NgModule({
   declarations: [HelloWorldWidgetComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, NoopAnimationsModule, MatGridListModule],
   providers: [],
   bootstrap: [],
   entryComponents: [HelloWorldWidgetComponent],
--- widgets/microfront-a/src/app/hello-world-widget/hello-world-widget.component.ts
+++ widgets/microfront-a/src/app/hello-world-widget/hello-world-widget.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+export interface Tile {
+  color: string;
+  cols: number;
+  rows: number;
+  text: string;
+}
 
 @Component({
   selector: 'app-hello-world-widget',
   templateUrl: './hello-world-widget.component.html',
-  styleUrls: ['./hello-world-widget.component.scss']
+  styleUrls: ['./hello-world-widget.component.scss'],
 })
-export class HelloWorldWidgetComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class HelloWorldWidgetComponent {
+  tiles: Tile[] = [
+    { text: 'One', cols: 3, rows: 1, color: 'lightblue' },
+    { text: 'Two', cols: 1, rows: 2, color: 'lightgreen' },
+    { text: 'Three', cols: 1, rows: 1, color: 'lightpink' },
+    { text: 'Four', cols: 2, rows: 1, color: '#DDBDF1' },
+  ];
 }
